c55a2389aa78f70b4ba3d2eb032c3b81c129cd19
lukaslukas <eiglerhardingl@gmail.com>
2019-06-04 11:46:26 -0400

    updating css overflow property updates

--- wp-content/themes/archtober19/assets/js/script.js
+++ wp-content/themes/archtober19/assets/js/script.js
@@ -175,7 +175,7 @@ Site.calendar = function(){
 
 			var scrollDistance = $("#october_15").offset().top
 			console.log(scrollDistance)
-			$("html, body").animate({
+			$(window).animate({
 				scrollTop: scrollDistance
 			}, 1000)
 			
