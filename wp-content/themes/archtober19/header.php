8ebd54f1bb46a1d45f0bf74120b12554ac0616c7
lukaslukas <eiglerhardingl@gmail.com>
2019-06-04 11:51:33 -0400

    updated header to be user-scalable

--- wp-content/themes/archtober19/header.php
+++ wp-content/themes/archtober19/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0">
 	<title><?php bloginfo( 'name' ); ?></title>
 	<?php wp_head(); ?>
 
