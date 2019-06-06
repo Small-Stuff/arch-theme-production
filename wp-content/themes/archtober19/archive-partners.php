88e82290a9dcff3bf74301ef96dd050a88b85947
lukaslukas <eiglerhardingl@gmail.com>
2019-06-06 11:16:32 -0400

    slight bugs and updates before demo

--- wp-content/themes/archtober19/archive-partners.php
+++ wp-content/themes/archtober19/archive-partners.php
@@ -8,7 +8,7 @@ Template Name: Partners Archive
 <?php get_template_part('components/header', 'index') ?>
 <section class="index_section_titles index_partners">
 	<h2 class="index_section_title section_three event_title arch_partner">Partner</h2>
-	<h2 class="index_section_title section_six arch_partner">Website</h2>
+	<h2 class="index_section_title section_six arch_partner mobile_hide">Website</h2>
 	<h2 class="index_section_title section_six arch_partner">Phone</h2>
 </section>
 <?php get_template_part('components/loop', 'partners'); ?>
--- wp-content/themes/archtober19/components/row-partners.php
+++ wp-content/themes/archtober19/components/row-partners.php
@@ -4,7 +4,7 @@
 	$subphones = '';
 	if( have_rows('sub_partner') ):
     while ( have_rows('sub_partner') ) : the_row();
-      $subnames .= '<span class="sub_section">'.get_sub_field('sub_partner_name').'</span>';
+      $subnames .= '<span class="sub_section mobile_hide">'.get_sub_field('sub_partner_name').'</span><a class="sub_section mobile_reveal" href="'.get_sub_field('sub_partner_website').'" >'.get_sub_field('sub_partner_name').'</a>';
       $subwebsites .= (get_sub_field('sub_partner_website')) ? '<a href="'.get_sub_field('sub_partner_website').'" class="sub_section">'.pretty_url(get_sub_field('sub_partner_website')).'</a>' : '<span class="sub_section">–</span>';
       $subphones .= (get_sub_field('sub_partner_phone')) ? '<a href="tel:+'.get_sub_field('sub_partner_phone').'" class="sub_section">'.get_sub_field('sub_partner_phone').'</a>': '<span class="sub_section">–</span>';
     endwhile;
@@ -12,10 +12,11 @@
 ?>
 <div class="index_section arch_partners">
 	<h3 class="index_section_title section_three event_title arch_partner">
-		<?= get_the_title(); ?>
+		<span class="mobile_hide"><?= get_the_title(); ?></span>
+		<a target="_blank" class="mobile_reveal" href="<?= get_field('website') ?>"><?= get_the_title(); ?></a>
 		<?= $subnames; ?>
 		</h3>
-	<h3 class="index_section_title section_six arch_partner">
+	<h3 class="index_section_title section_six arch_partner mobile_hide">
 		<a target="_blank" href="<?= get_field('website') ?>"><?= pretty_url(get_field('website')) ?></a>
 		<?= $subwebsites; ?>
 	</h3>
