e352500b74ffa606dc9287e73a0d6937c625b3d9
Lukas Eigler-Harding <eiglerhardingl@gmail.com>
2019-06-27 17:04:58 +0200

    updated sponsors

--- wp-content/themes/archtober19/components/row-sponsors.php
+++ wp-content/themes/archtober19/components/row-sponsors.php
@@ -1,5 +1,5 @@
 <?php if(get_field('sponsor_logo')): 
-	?><?= (get_field('sponsor_website')) ? '<a class="sponsor_display_'.get_field('sponsor_display_size').'" target="_blank" href="'.get_field('sponsor_website').'"><img src="'.get_field('sponsor_logo').'"></a>' : '<img src="'.get_field('sponsor_logo').'">';
+	?><?= (get_field('sponsor_website')) ? '<a class="sponsor_display_'.get_field('sponsor_display_size').'" target="_blank" href="'.get_field('sponsor_website').'"><img src="'.get_field('sponsor_logo').'"></a>' : '<img class="sponsor_display_'.get_field('sponsor_display_size').'" src="'.get_field('sponsor_logo').'">';
 ?><?php else: 
 ?><h1 class="sponsor_display_<?= get_field('sponsor_display_size') ?>"><?= (get_field('sponsor_website')) ? '<a target="_blank" href="'.get_field('sponsor_website').'">'.get_the_title().'</a>' : get_the_title() ?></h1
 ><?php endif; ?>
