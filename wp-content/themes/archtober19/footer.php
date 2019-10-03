7e564ec854fcd8f36db4a9d2e5d99dfe1086b821
lukaslukas <eiglerhardingl@gmail.com>
2019-10-03 10:44:38 -0400

    updated footer with credits

--- wp-content/themes/archtober19/footer.php
+++ wp-content/themes/archtober19/footer.php
@@ -5,6 +5,7 @@
 			><section class="footer_column">
 				<?= wpautop(the_field('footer_right_column', 'option')) ?>
 			</section>
+			<aside class="footer_credits">Design by <a target="_blank" href="https://smallstuff.us/">Small Stuff</a> with <a target="_blank" href="https://lukas.eigler-harding.com">Lukas Eigler-Harding</a></aside>
 		</footer>
 	</main>
 </section>
